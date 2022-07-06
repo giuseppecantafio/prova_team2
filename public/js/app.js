e5583143eededc2ada67593b245dd95e0cdf6e48
naiadidub <antonio.zaghi1405@gmail.com>
2022-07-06 16:00:37 +0200

    aggiunta titolo

--- public/js/app.js
+++ public/js/app.js
@@ -1950,7 +1950,7 @@ var staticRenderFns = [function () {
     staticClass: "card-header"
   }, [_vm._v("Example Component")]), _vm._v(" "), _c("div", {
     staticClass: "card-body"
-  }, [_vm._v("\n                        I'm an example component.\n                    ")])])])])]);
+  }, [_vm._v("\r\n                        I'm an example component.\r\n                    ")])])])])]);
 }];
 render._withStripped = true;
 
@@ -49487,8 +49487,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\MAMP\htdocs\esercizi_php\luglio\prova_team2\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\MAMP\htdocs\esercizi_php\luglio\prova_team2\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\Antonio\Desktop\deliveboo\prova_team2\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\Antonio\Desktop\deliveboo\prova_team2\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
